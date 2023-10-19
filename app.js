3ca5aaa8eb32bb6cbc9a4e59bd8a7a569b74005f
Keerthana R <122580245+Keerthana-keerthu@users.noreply.github.com>
2023-10-19 16:56:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 function validateForm(event) {
-    event.preventDefault();
-    const username = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const usernameError = document.getElementById("usernameError");
-    const emailError = document.getElementById("emailError");
-    const passwordError = document.getElementById("passwordError");
+  event.preventDefault();
+  const username = document.getElementById("username");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const usernameError = document.getElementById("usernameError");
+  const emailError = document.getElementById("emailError");
+  const passwordError = document.getElementById("passwordError");
+  const nameRegex = /^[A-Za-z]+$/;
+  const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+  usernameError.innerText = "";
+  emailError.innerText = "";
+  passwordError.innerText = "";
 
-    
-    usernameError.textContent = "";
-    emailError.textContent = "";
-    passwordError.textContent = "";
-
-    if (username.trim() === "") {
-        usernameError.textContent = "Username is required";
-        return;
-    }
-
-    if (email.trim() === "") {
-        emailError.textContent = "Email is required";
-        return;
-    }
-
-    if (password.trim() === "") {
-        passwordError.textContent = "Password is required";
-        return;
-    }
-
-    
-    window.location.href = "./signin.html";
+  if (!nameRegex.test(username.value)) {
+      usernameError.innerText = "Username should only contain letters.";
+      return;
+  }
+  if (!emailRegex.test(email.value)) {
+      emailError.innerText = "Please enter a valid email address.";
+      return;
+  }
+  const userData = {
+      username: username.value,
+      email: email.value,
+      password: password.value,
+  };
+  localStorage.setItem("userData", JSON.stringify(userData));
+  goToSignInPage();
 }
 
+function goToSignInPage() {
+  window.location.href = "./signin.html";
+}
